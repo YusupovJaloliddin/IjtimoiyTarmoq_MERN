b183b30bd1ba7810d08459574a9f4c978d7b5ed1
YusupovJaloliddin <yusupovjaloliddin4633@gmail.com>
2022-02-17 13:00:50 +0500

    Ijtimoy tarmoq loyiha

--- config/production.js
+++ config/production.js
@@ -2,5 +2,3 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
 };
-// "mongodb+srv://jalol:37qGQzoz5CRfbNyD@cluster0.ieol0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-// "yusupovjaloliddin4633"
